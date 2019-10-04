490a5e464208cf42821959e833e73b7ee3180aa8
Hao Li <hayatelee@gmail.com>
2019-10-04 15:21:12 +0800

    Add a trick to workaround an overlay issus in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-set -x
-set -e
+set -xe
 
-mount -t debugfs debugfs /sys/kernel/debug
+# enable debug settings
 echo 0 > /proc/sys/kernel/kptr_restrict
+mount -t debugfs debugfs /sys/kernel/debug
+
+# a trick to workaround an overlay fs issue in container
+mkdir -p ~/bin
+mount -t tmpfs -o size=5m tmpfs ~/bin
+cp `which bpftrace` ~/bin/
+export PATH=~/bin:$PATH
 
 exec "$@"
 
