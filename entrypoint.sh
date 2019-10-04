f40dad03c823af0e5b8437fb56cd7bdce064b719
Hao Li <hayatelee@gmail.com>
2019-10-04 19:26:33 +0800

    Adjust entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ echo 0 > /proc/sys/kernel/kptr_restrict
 mount -t debugfs debugfs /sys/kernel/debug
 
 # a trick to workaround an overlay fs issue in container
-mkdir -p ~/bin
-mount -t tmpfs -o size=5m tmpfs ~/bin
-cp `which bpftrace` ~/bin/
-export PATH=~/bin:$PATH
+mkdir -p "$HOME/bin"
+mount -t tmpfs -o size=5m tmpfs "$HOME/bin"
+cp `which bpftrace` "$HOME/bin/"
+export PATH="$HOME/bin:$PATH"
 
 exec "$@"
 
